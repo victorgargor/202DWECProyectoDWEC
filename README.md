52cabd5fd9efe8b380fda94036f54083c62db975
victorgargor <victor.gargor@educa.jcyl.es>
2024-11-25 10:47:58 +0100

    25/11/2024 Primer Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# 202DWECProyectoDWEC
+
+## Asignatura: Desarrollo Web en Entorno Cliente
+
+- **Autor**: Víctor García Gordón
+- **Fecha Inicio Proyecto**: 25/11/2024
+- **Última Actualización**: 25/11/2024
+
+---
+
+## Descripción
+
+Proyecto de **Desarrollo Web en Entorno Cliente** centrado en el uso de **JavaScript** para el desarrollo de funcionalidades del lado del cliente.
+
+---
+
+## Contenidos del Curso
+
+### TEMA 1: SELECCIÓN DE ARQUITECTURAS Y HERRAMIENTAS DE PROGRAMACIÓN
+
+### TEMA 2: MANEJO DE LA SINTAXIS DEL LENGUAJE
+
+### TEMA 3: OBJETOS, FUNCIONES Y ESTRUCTURAS
+
+### TEMA 4: EL MODELO DE OBJETOS DEL DOCUMENTO (DOM Y BOM)
+
+---
+
+## Tecnologías y Herramientas
+
+### Frontend
+
+- **HTML**
+- **CSS**
+- **JavaScript**
+
+---
+
+## Entornos
+
+### Entorno de Desarrollo (Local)
+
+- URL: [http://daw202.isauces.local/202DWECProyectoDWEC](http://daw202.isauces.local/202DWECProyectoDWEC)
+
+### Entorno de Explotación
+
+---
+
+## Notas
+
+- Este proyecto está en desarrollo y puede haber actualizaciones o mejoras futuras.
+
---
+++ indexProyectoDWEC.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="webroot/css/index.css" type="text/css">
+    <title>Víctor García Gordón</title>
+</head>
+<body>
+    <header>      
+        <h1 id="inicio">Desarrollo Web en Entorno Cliente</h1>
+    </header>
+    <main>
+        <nav>
+            <h2>TEMA 1 SELECCIÓN DE ARQUITECTURAS Y HERRAMIENTAS DE PROGRAMACIÓN</h2>
+            <ul>
+                <li><a href="">Ejercicios Tema 1</a></li>
+            </ul>
+            <h2>TEMA 2 MANEJO DE LA SINTAXIS DEL LENGUAJE</h2>
+            <ul>
+                <li><a href="">Ejercicios Tema 1</a></li>
+            </ul>
+            <h2>TEMA 3 OBJETOS, FUNCIONES Y ESTRUCTURAS</h2>
+            <ul>
+                <li><a href="">Ejercicios Tema 1</a></li>
+            </ul>
+            <h2>TEMA 4 EL MODELO DE OBJETOS DEL DOCUMENTO (DOM Y BOM)</h2>
+            <ul>
+                <li><a href="">Ejercicios Tema 1</a></li>
+            </ul>           
+        </nav>       
+    </main>
+    <footer>
+        <div>
+            <a href="/index.html">Víctor García Gordón</a>
+            <a target="blank" href="doc/curriculum.pdf"><img src="doc/curriculum.jpg" alt="curriculum"></a>
+            <a target="blank" href="https://github.com/victorgargor/202DWECProyectoDWEC"><img src="doc/github.png" alt="github"></a>
+            <a target="blank" href="https://github.com">Web Imitada</a>
+        </div>
+    </footer>
+</body>
+</html>
